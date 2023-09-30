662d47ad34d0e89e694c8ac8add2ffd62fc0d3e1
Siddhi Manglam <76934290+Siddhi-ux@users.noreply.github.com>
2023-09-30 23:32:25 +0530

    Update Median of two array.java

--- Median of two array.java
+++ Median of two array.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
         int[] num1 =new int[n];
